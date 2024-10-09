c4dc32fad461bd9277b3d2e2fb2952c08ff2fc8f
Suzuki <joaosuzuki98@outlook.com>
2024-10-09 20:12:20 -0300

    style(main-page): Add grid layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import styles from './App.module.css';
+import './App.css';
 
 function App() {
   return (
-    <div className={styles.mainContainer}>
-      Tasks
+    <div className="main-container">
+      <div className="bg-slate-300">teste</div>
+      <div className="bg-slate-300">testes</div>
+      <div className="bg-slate-300">testeste</div>
+      <div className="bg-slate-300">testes</div>
+      <div className="bg-slate-300">testestetes</div>
+      <div className="bg-slate-300">testeste</div>
+      <div className="bg-slate-300">testes</div>
+      <div className="bg-slate-300">teste</div>
+      <div className="bg-slate-300">a</div>
+      <div className="bg-slate-300">a</div>
+      <div className="bg-slate-300">a</div>
+      <div className="bg-slate-300">a</div>
+      <div className="bg-slate-300">a</div>
+      <div className="bg-slate-300">a</div>
     </div>
   );
 }
