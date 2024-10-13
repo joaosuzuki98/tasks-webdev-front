889707857f5374bb62fe1ecc38078e68da33c27f
Suzuki <joaosuzuki98@outlook.com>
2024-10-13 12:30:48 -0300

    feat(tasks): Add modal to create task

--- src/components/AddButton/AddButton.js
+++ src/components/AddButton/AddButton.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function AddButton() {
+function AddButton({ onClick }) {
   return (
-    <div className='mt-[40px] flex flex-col items-center justify-center w-full h-[145px] bg-light-green rounded-2xl outline-2 outline-dashed outline-[#56E39F]'>
+    <div onClick={onClick} className='mt-[40px] flex flex-col items-center justify-center w-full h-[145px] bg-light-green rounded-2xl outline-2 outline-dashed outline-[#56E39F] cursor-pointer'>
       <div className='rounded-full bg-main-green w-8 h-8 flex items-center justify-center'>
         <i class="color-dark-green fa-solid fa-plus"></i>
       </div>
--- src/components/TaskBlock/TaskBlock.js
+++ src/components/TaskBlock/TaskBlock.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import styles from './TaskBlock.module.css'
 
-function TaskBlock({dayName, children}) {
+function TaskBlock({dayName, children, border, position}) {
   return (
-    <div className='col-span-1 px-[20px] border-r-2 border-[#373942] flex flex-col h-full'>
+    <div className={`col-span-1 px-[20px] border-r-2 border-[#373942] flex flex-col h-full ${border} ${position} ${styles.addButtonContainer}`}>
         <h3 className='text-xl'>
             {dayName}
         </h3>
 
-        <div className='flex flex-col items-center'>
+        <div className={`flex flex-col items-center ${styles.addButtonWrapper}`}>
             {children}
         </div>
-    </div> 
+    </div>
   )
 }
 
